e1fe83832eae08074986d0cb530bfee2f27f6654
Marc Bischof <marc.bischof@swisscom.com>
2019-02-19 15:02:45 +0100

    Remove unnecessary outputs during pre-runtime hook

--- .profile.d/setenv.sh
+++ .profile.d/setenv.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-echo ".profile.d/setenv.sh"
 echo "Turn VCAP_SERVICES into flat env vars"
-./vcap-squash-linux-amd64
 eval $(./vcap-squash-linux-amd64)
-echo "Verify:"
-export TEST=WORKING
-echo $TEST
-echo $NEOS_FLOW_DEMO_DB_HOST
